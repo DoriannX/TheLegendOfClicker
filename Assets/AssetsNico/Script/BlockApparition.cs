f92d561f780ed8d5cdee125f1abee1dd4e27ebc1
NicolasTre <N.TREZEGUET@lahorde.school>
2024-04-12 15:43:25 +0200

    lvl system

--- Assets/AssetsNico/Script/BlockApparition.cs
+++ Assets/AssetsNico/Script/BlockApparition.cs
@@ -5,11 +5,12 @@ public class BlockApparition : MonoBehaviour
 {
     [SerializeField] private Image square;
 
-
+    
     public void OnClick()
     {
         square.transform.position = new Vector3(Random.Range(-5, 5), Random.Range(-3, 3), 0);
         ScoreManager.scoreCount++;
+        GainXP.instance.GainEXP();
     }
 
 
